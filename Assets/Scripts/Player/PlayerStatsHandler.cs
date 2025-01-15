9aae9567dffaa75f45985c5b0483384ba5978a41
NotJstn1026 <justinmartin822@gmail.com>
2025-01-15 11:47:51 +0100

    PlayerScript hinzugefügt

--- Assets/Scripts/Player/PlayerStatsHandler.cs
+++ Assets/Scripts/Player/PlayerStatsHandler.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStatsHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action OnDie;
+    private int healthPoints = 1;
+    public int HealthPoints
     {
-        
+        get { return healthPoints; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Meteor")
+        {
+            if (healthPoints <= 0)
+            {
+                OnDie();
+            }
+        }
     }
 }
