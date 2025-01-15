23e50a9be280fcab24b67b27031b9ebd03f0fcad
Sell1412 <tobias.hoeflmeier@web.de>
2025-01-15 11:59:14 +0100

    Script Movement added, Borders Added

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector2 velocity;
+
+    private float movespeed = 200f;
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    private void FixedUpdate()
+    {
+         rb2D.velocity = velocity * Time.deltaTime;
+    }
+
+    private void Movement()
+    {
+        velocity = Vector2.zero;
+
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        velocity = new Vector2(horizontalInput * movespeed, verticalInput * movespeed);
+    }
+}
